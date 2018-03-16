2908f0f87c011b3269462053fbaf7b64af960e9d
Tuseeva Elena <etuseeva@griddynamics.com>
2018-03-16 14:41:17 +0400

    Copy lesson2 -> lesson5

---
+++ src/com/epam/lena_tuseeva/java/lesson5/task1/Salad/Vegetable.java
@@ -0,0 +1,32 @@
+package com.epam.lena_tuseeva.java.lesson5.task1.Salad;
+
+public class Vegetable {
+    private Integer calories = null;
+    private Integer weight = null;
+
+    public Vegetable(Integer calories, Integer weight) {
+        this.calories = calories;
+        this.weight = weight;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "[Object] Vegetable";
+    }
+}
