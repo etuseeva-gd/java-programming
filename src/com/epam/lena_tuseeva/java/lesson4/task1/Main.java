c1104754b34df7e5250fb8830400b406a68bdca8
Tuseeva Elena <etuseeva@griddynamics.com>
2018-04-27 18:06:22 +0400

    ...

--- src/com/epam/lena_tuseeva/java/lesson4/task1/Main.java
+++ src/com/epam/lena_tuseeva/java/lesson4/task1/Main.java
@@ -17,27 +17,21 @@ public class Main {
         try {
             tomato = new Tomato(caloriesNegative, weightPositive, Tomato.TypeTomato.Simple);
         } catch (NegativeCaloriesException | NegativeWeightException e) {
-            System.out.println("Tomato':" + e.getMessage());
+            System.out.println("Tomato: " + e.getMessage());
         }
 
         Cucumber cucumber = null;
         try {
             cucumber = new Cucumber(caloriesPositive, weightNegative, Cucumber.StateCucumber.Fresh);
         } catch (NegativeCaloriesException | NegativeWeightException e) {
-            System.out.println("Cucumber':" + e.getMessage());
+            System.out.println("Cucumber: " + e.getMessage());
         }
 
         Bow bow = null;
         try {
             bow = new Bow(caloriesPositive, caloriesPositive, Bow.TypeBow.Bulbous);
         } catch (NegativeCaloriesException | NegativeWeightException e) {
-            System.out.println("Bow':" + e.getMessage());
-        }
-
-        if (tomato != null) {
-            System.out.println("Tomato was successfully created!");
-        } else {
-            System.out.println("Tomato wasn't successfully created!");
+            System.out.println("Bow: " + e.getMessage());
         }
 
         Salad salad = null;
@@ -47,16 +41,35 @@ public class Main {
             System.out.println("Salad: " + e.getMessage());
         }
 
+        //Recreate salad for next examples
+        salad = new Salad(3);
+
         if (salad != null) {
             System.out.println("Salad was successfully created!");
-        } else {
-            System.out.println("Salad wasn't successfully created!");
 
             int i = 0;
-            salad.add(++i, cucumber);
-            salad.add(++i, bow);
-            salad.add(++i, tomato);
+            if (cucumber != null) {
+                System.out.println("Cucumber was successfully created!");
+                salad.add(++i, cucumber);
+            } else {
+                System.out.println("Cucumber wasn't successfully created!");
+            }
+
+            if (bow != null) {
+                System.out.println("Bow was successfully created!");
+                salad.add(++i, bow);
+            } else {
+                System.out.println("Bow wasn't successfully created!");
+            }
+
+            if (tomato != null) {
+                System.out.println("Tomato was successfully created!");
+                salad.add(++i, tomato);
+            } else {
+                System.out.println("Tomato wasn't successfully created!");
+            }
+        } else {
+            System.out.println("Salad wasn't successfully created!");
         }
     }
-
 }
