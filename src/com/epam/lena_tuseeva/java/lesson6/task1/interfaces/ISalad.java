66556d42c96666813a293ba371b3b361dafa177a
Tuseeva Elena <etuseeva@griddynamics.com>
2018-04-27 18:15:09 +0400

    Move code of 5 lesson to 6

---
+++ src/com/epam/lena_tuseeva/java/lesson6/task1/interfaces/ISalad.java
@@ -0,0 +1,13 @@
+package com.epam.lena_tuseeva.java.lesson6.task1.interfaces;
+
+import com.epam.lena_tuseeva.java.lesson6.task1.models.Vegetable;
+
+import java.util.List;
+
+public interface ISalad {
+    public void add(Vegetable vegetable);
+    public void sort();
+
+    public int getSaladCalories();
+    public List<Vegetable> findVegetablesByWeight(int min, int max);
+}
---
+++ src/com/epam/lena_tuseeva/java/lesson6/task1/models/Cucumber.java
@@ -0,0 +1,30 @@
+package com.epam.lena_tuseeva.java.lesson6.task1.models;
+
+public class Cucumber extends Vegetable {
+    public enum StateCucumber {
+        Fresh,
+        Salty,
+        Marinated,
+    }
+
+    private StateCucumber state = null;
+
+    public Cucumber(Integer calories, Integer weight, StateCucumber state) {
+        super(calories, weight);
+
+        this.state = state;
+    }
+
+    public void setState(StateCucumber state) {
+        this.state = state;
+    }
+
+    public StateCucumber getState() {
+        return state;
+    }
+
+    @Override
+    public String toString() {
+        return "[Object] Cucumber";
+    }
+}
